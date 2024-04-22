6ba71aaf28ce049b456afdf1fceead2ce5e54a04
www-jong <weon1009@gmail.com>
2024-04-23 02:14:44 +0900

    Init: 프로젝트 초기설정

---
+++ Backend/moass-api-server/src/main/java/com/moass/api/MoassApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.moass.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoassApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoassApiServerApplication.class, args);
+    }
+
+}
