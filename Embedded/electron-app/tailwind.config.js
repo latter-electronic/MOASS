6ba2d8e908d58e4511f0ce1dd5a7e03ddc6cece2
RosaDamascena <hsj990604@gmail.com>
2024-04-23 14:53:01 +0900

    Init: tailwind 추가

---
+++ Embedded/electron-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/renderer/index.html',
+    './src/renderer/src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
