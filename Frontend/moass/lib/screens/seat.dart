21b7eaf85724358c645099d40c0578a8f316ceff
unknown <chiru7080@daum.net>
2024-04-23 23:15:15 +0900

    chore: 스크린 추가

---
+++ Frontend/moass/lib/screens/seat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Seat extends StatelessWidget {
+  const Seat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Work'));
+  }
+}
