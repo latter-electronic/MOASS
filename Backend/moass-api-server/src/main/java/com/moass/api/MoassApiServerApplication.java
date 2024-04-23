da2cdd16054736e1cae81955ec172ee97dd13049
www-jong <weon1009@gmail.com>
2024-04-23 11:23:07 +0900

    Add: BlockHound 세팅

--- Backend/moass-api-server/src/main/java/com/moass/api/MoassApiServerApplication.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/MoassApiServerApplication.java
@@ -1,13 +1,17 @@
 package com.moass.api;
 
+import com.moass.api.global.config.BlockHoundCustomConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound. BlockHound;
 
 @SpringBootApplication
 public class MoassApiServerApplication {
 
     public static void main(String[] args) {
+        BlockHound.install(new BlockHoundCustomConfiguration());
         SpringApplication.run(MoassApiServerApplication.class, args);
     }
+
 
 }
