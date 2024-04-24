954d561cf91ef34d29a0d1826f41ae2564595c67
서지수 <diduedidue@naver.com>
2024-04-24 12:00:53 +0900

    Feat: user 정보 불러오는 api 임의 연결

--- Embedded/electron-app/src/renderer/src/components/Navbar.jsx
+++ Embedded/electron-app/src/renderer/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { fetchAllUsers } from '../services/userService.js';
 import useUIStore from '../stores/UIStore.js';
 
 import mainIcon from '../assets/navbar_icon_main.svg';
@@ -12,7 +14,21 @@ import alertIcon_w from '../assets/navbar_icon_alert_white.svg';
 import testProfileImg from '../assets/profileImageTest.jpg';
 
 export default function Navbar() {
-    const { activeTab, setActiveTab } = useUIStore();
+    const { activeTab, setActiveTab } = useUIStore();  // 네브바 선택 아이콘
+    const [users, setUsers] = useState([]); // 사용자 데이터 저장할곳
+
+    useEffect(() => {
+        const loadUsers = async () => {
+            try {
+                const userData = await fetchAllUsers();
+                setUsers(userData); // API 호출 결과를 상태에 저장
+            } catch (error) {
+                console.error('사용자 데이터를 불러오는 중 오류가 발생했습니다:', error);
+            }
+        };
+
+        loadUsers(); // 컴포넌트 마운트 시 사용자 데이터 불러오기
+    }, []); // 빈 의존성 배열을 제공하여 컴포넌트가 마운트될 때 한 번만 호출되도록 설정
 
     return (
         <nav className="flex flex-col justify-between w-24 h-screen bg-gray-800 text-white p-4">
