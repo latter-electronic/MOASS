bc648c0f959005ca80e351d66a2bda58a59f4484
서지수 <diduedidue@naver.com>
2024-04-24 22:22:16 +0900

    Feat: 시계/날짜 컴포넌트 구체화

--- Embedded/electron-app/src/renderer/src/pages/home/HomePage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/home/HomePage.jsx
@@ -3,20 +3,29 @@ import Calendar from './CalendarWidget.jsx'
 import TodoList from './TodoListComponent.jsx'
 import Schedule from './ScheduleCard.jsx'
 
+import Mozzy from '../../assets/Mozzy.svg'
+
 export default function HomePage() {
-    return(
-        <div>
-            <div className="container mx-auto p-4">
-                <Clock />
-                <div className="grid grid-cols-3 gap-4">
-                    <div>
-                        <Calendar />
+    return (
+        <div className="container mx-auto p-4 h-screen">
+            {/* 전체 레이아웃을 3열로 분할하고, 각 열을 화면 높이와 동일하게 설정 */}
+            <div className="grid grid-cols-3 gap-8 h-full">
+                {/* 1열: 시계, 달력 위아래로 배치 */}
+                <div className="flex flex-col justify-between">
+                    <Clock />
+                    <div className="flex justify-center">
+                    <Calendar />
                     </div>
-                    <div>
-                        <Schedule />
-                    </div>
-                    <div>
-                        <TodoList />
+                </div>
+                {/* 2열: 스케줄러만 중앙에 */}
+                <div className="flex flex-col justify-center">
+                    <Schedule />
+                </div>
+                {/* 3열: 할 일 목록, 모찌 위아래로 배치 */}
+                <div className="flex flex-col justify-between">
+                    <TodoList />
+                    <div className="flex justify-center">
+                        <img src={Mozzy} alt="Mozzy" className="size-64"/>
                     </div>
                 </div>
             </div>
