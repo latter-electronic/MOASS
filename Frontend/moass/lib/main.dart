68b0d6375ccb23351d9c4d4bb7dc790e96614cc8
jongmin Son <kain9101@naver.com>
2024-04-25 00:26:36 +0900

    Feat: home_user Screen 메인에 연결

--- Frontend/moass/lib/main.dart
+++ Frontend/moass/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moass/screens/home_user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Home(),
+    );
   }
 }
--- Frontend/moass/lib/screens/home_user.dart
+++ Frontend/moass/lib/screens/home_user.dart
@@ -5,6 +5,6 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Work'));
+    return const Scaffold(body: Center(child: Text('Work')));
   }
 }
