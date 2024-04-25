cca38452c7bfd98fd6300de529bf5153b079f245
손종민 <kain9101@naver.com>
2024-04-25 17:51:51 +0900

    Feat: 투두리스트 위젯 생성

---
+++ Frontend/moass/lib/widgets/check_box.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CheckboxWidget extends StatefulWidget {
+  const CheckboxWidget({super.key});
+
+  @override
+  State<CheckboxWidget> createState() => _CheckboxWidgetState();
+}
+
+class _CheckboxWidgetState extends State<CheckboxWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return const Color(0xFF6ECEF5);
+      }
+      return const Color(0xFF6ECEF5);
+    }
+
+    return Checkbox(
+      checkColor: Colors.black,
+      fillColor: MaterialStateProperty.resolveWith(getColor),
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
