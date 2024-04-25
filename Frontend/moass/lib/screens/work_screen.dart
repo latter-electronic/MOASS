2db862f94b2790fbfdaf03d5eaae8c054244fdc0
장현욱 <chiru7080@daum.net>
2024-04-25 17:54:42 +0900

    Feat: 업무 페이지 AppBar 기초틀

--- Frontend/moass/lib/screens/work_screen.dart
+++ Frontend/moass/lib/screens/work_screen.dart
@@ -5,6 +5,32 @@ class WorkScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('업무'));
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        elevation: 2,
+        backgroundColor: Colors.blue.shade900,
+        foregroundColor: Colors.blue.shade100,
+        title: Row(
+          mainAxisSize:
+              MainAxisSize.min, // 이 속성으로 인해 아이템들이 타이틀 중간이 아닌 시작 부분에 위치
+          children: [
+            Icon(Icons.work_outline, color: Colors.blue.shade100), // 아이콘 색상 조정
+            const SizedBox(width: 10), // 아이콘과 텍스트 사이 간격
+            const Text(
+              "업무",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: false,
+      ),
+      body: const Center(
+        child: Text('Work Screen Content'),
+      ),
+    );
   }
 }
