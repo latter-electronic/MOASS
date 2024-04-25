ab73a6621853b68674f09eeb4f09b21a84bc009d
손종민 <kain9101@naver.com>
2024-04-25 15:44:01 +0900

    Fix: appBar 구조 각 페이지 내로 들어가도록 분리

--- Frontend/moass/lib/screens/main_home_screen.dart
+++ Frontend/moass/lib/screens/main_home_screen.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:moass/widgets/top_bar.dart';
 
 class MainHomeScreen extends StatelessWidget {
   const MainHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CategoryText(),
-        SizedBox(
-          height: 5,
-        ),
-        Text('내 상태 설정'),
-        Text('다음 일정'),
-        Text('할 일 목록'),
-        Text('오늘 내 예약'),
-      ],
+    return const Scaffold(
+      appBar: TopBar(title: '메인'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CategoryText(),
+          SizedBox(
+            height: 5,
+          ),
+          Text('내 상태 설정'),
+          Text('다음 일정'),
+          Text('할 일 목록'),
+          Text('오늘 내 예약'),
+        ],
+      ),
     );
   }
 }
