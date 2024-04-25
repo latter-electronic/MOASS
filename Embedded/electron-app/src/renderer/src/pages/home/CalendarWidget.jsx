a4f7724e9215509b7d5d42bcb1cb2b911e22bbd9
서지수 <diduedidue@naver.com>
2024-04-25 09:50:23 +0900

    Feat: 홈 스케줄 스크롤바 설정

--- Embedded/electron-app/src/renderer/src/pages/home/CalendarWidget.jsx
+++ Embedded/electron-app/src/renderer/src/pages/home/CalendarWidget.jsx
@@ -3,7 +3,7 @@ import Calendar from "../../assets/Calendar.png"
 export default function CalendarWidget() {
     return(
         <>
-            <div class="flex items-center justify-center">
+            <div className="flex items-center justify-center">
             <img src={ Calendar } alt="캘린더" className="w-56 rounded ml-6 mb-6"/>
         </div>
         </>
--- Embedded/electron-app/src/renderer/src/pages/home/HomePage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/home/HomePage.jsx
@@ -7,7 +7,7 @@ import Mozzy from '../../assets/Mozzy.svg'
 
 export default function HomePage() {
     return (
-        <div className="container mx-auto p-4 h-screen">
+        <div className=" mx-auto p-4 h-screen">
             {/* 전체 레이아웃을 3열로 분할하고, 각 열을 화면 높이와 동일하게 설정 */}
             <div className="grid grid-cols-[1fr,2.5fr,1fr] gap-8 h-full">
                 {/* 1열: 시계, 달력 위아래로 배치 */}
