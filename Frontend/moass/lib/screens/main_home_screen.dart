13727469909be05571cb5ef580ebd5f2aa9d62dc
손종민 <kain9101@naver.com>
2024-04-25 16:12:03 +0900

    Feat: CategoryText widget 생성 및 분리

--- Frontend/moass/lib/screens/main_home_screen.dart
+++ Frontend/moass/lib/screens/main_home_screen.dart
@@ -1,40 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:moass/widgets/top_bar.dart';
+
+import '../widgets/category_text.dart';
 
 class MainHomeScreen extends StatelessWidget {
   const MainHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TopBar(title: '메인'),
+    return Scaffold(
+      appBar: const TopBar(title: '메인'),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CategoryText(),
-          SizedBox(
+          const Flexible(
+            flex: 2,
+            child: CategoryText(
+              text: '내 기기 상태',
+            ),
+          ),
+          const SizedBox(
             height: 5,
           ),
-          Text('내 상태 설정'),
-          Text('다음 일정'),
-          Text('할 일 목록'),
-          Text('오늘 내 예약'),
+          Flexible(
+            flex: 10,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                      decoration: const BoxDecoration(color: Colors.grey),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 90.0),
+                        child: CategoryText(text: '기기 이미지 들어갈 곳'),
+                      )),
+                ),
+              ],
+            ),
+          ),
+          const Flexible(flex: 2, child: CategoryText(text: '내 상태 설정')),
+          const Flexible(flex: 2, child: CategoryText(text: '다음 일정')),
+          const Flexible(flex: 2, child: CategoryText(text: '할 일 목록')),
+          const Flexible(flex: 2, child: CategoryText(text: '오늘 내 예약')),
         ],
       ),
-    );
-  }
-}
-
-class CategoryText extends StatelessWidget {
-  const CategoryText({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text(
-      '내 기기 상태',
-      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
     );
   }
 }
