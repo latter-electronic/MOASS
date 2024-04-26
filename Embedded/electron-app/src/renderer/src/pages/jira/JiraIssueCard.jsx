be0c29d3e12a2bb000ff76785f7838b2be6c7a33
서지수 <diduedidue@naver.com>
2024-04-26 12:03:15 +0900

    Feat: 지라페이지 UI 구현 완료

--- Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
+++ Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
@@ -1,5 +1,29 @@
+import IssueIcon from '../../assets/images/jira/jiraIssueIcon.svg'
+import profileImg from '../../assets/images/jira/jiraProfileImg1.png'
+import highestIcon from '../../assets/images/jira/p-highest.svg'
+import highIcon from '../../assets/images/jira/p-high.svg'
+import mediumIcon from '../../assets/images/jira/p-medium.svg'
+import lowIcon from '../../assets/images/jira/p-low.svg'
+import lowestIcon from '../../assets/images/jira/p-low.svg'
+
 export default function JiraIssueCard() {
     return (
-        <div></div>
+        <div className="flex flex-col mt-3 bg-white rounded-lg shadow p-3 px-5 gap-1">
+
+            <div className="font-medium text-gray-700">Electron 개발 초기 환경 테스트</div>
+            
+            <div className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                    <img src={IssueIcon} alt="jira issue icon" className="w-6 h-6" />
+                    <span className="bg-emerald-200 text-emerald-900 text-sm px-2 py-1 rounded">[EM] 임베디드 공통</span>
+                </div>
+                <div className="flex items-center gap-2 mt-1">
+                    <span className="bg-gray-200 rounded-full text-xs font-semibold px-2 py-1 text-gray-700">2</span>
+                    <img src={mediumIcon} alt="중간중요도" className="size-5"/>
+                    <img src={profileImg} alt="지라프로필1" className="rounded-full w-9 h-9" />
+                </div>
+            </div>
+
+        </div>
     );
 }
--- Embedded/electron-app/src/renderer/src/pages/jira/JiraPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/jira/JiraPage.jsx
@@ -2,38 +2,38 @@ import IssueCard from './JiraIssueCard.jsx';
 
 export default function JiraPage() {
     return (
-        <div className=" mx-auto p-4 h-screen w-90vh">
+        <div className=" mx-auto p-6 h-screen">
             <div className="text-white font-bold text-2xl mb-5">S10P31E203 보드</div>
-            <div className="grid grid-cols-3 gap-8 h-full">
-            
-            
-                {/* Todo Column */}
-                <div>
-                    <div className="bg-gray-700 p-3">
-                        <h3 className="text-white text-lg">Todo</h3>
+            <div className="grid grid-cols-3 gap-5 h-full w-[88vw] ml-3">
+
+                {/* 해야 할 일 */}
+                <div className="flex flex-col">
+                    <div className="bg-white/10 p-3 rounded-lg flex-1">
+                        <h3 className="text-white/70 text-lg ml-1 font-light">해야 할 일</h3>
                         <div className="mt-2">
-                            {/* Place the IssueCard Component here */}
+                            <IssueCard />
+                            <IssueCard />
                             <IssueCard />
                         </div>
                     </div>
                 </div>
 
-                {/* Now Column */}
-                <div className="flex-1">
-                    <div className="bg-gray-700 p-3">
-                        <h3 className="text-white text-lg">Now</h3>
+                {/* 진행 중 */}
+                <div className="flex flex-col">
+                    <div className="bg-white/10 p-3 rounded-lg flex-1">
+                        <h3 className="text-white/70 text-lg ml-1 font-light">진행 중</h3>
                         {/* Place for current tasks */}
                     </div>
                 </div>
 
-                {/* Done Column */}
-                <div className="flex-1">
-                    <div className="bg-gray-700 p-3">
-                        <h3 className="text-white text-lg">Done</h3>
+                {/* 완료 */}
+                <div className="flex flex-col">
+                    <div className="bg-white/10 p-3 rounded-lg flex-1">
+                        <h3 className="text-white/70 text-lg ml-1 font-light">완료</h3>
                         {/* Place for completed tasks */}
                     </div>
                 </div>
-            
+
             </div>
         </div>
     );
