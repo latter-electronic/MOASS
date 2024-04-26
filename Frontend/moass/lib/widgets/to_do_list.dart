ab85aad325d54120a742fe4e3ae22045ecedaa87
손종민 <kain9101@naver.com>
2024-04-26 16:53:06 +0900

    Feat: 투두리스트 인풋 폼 UI 구현

--- Frontend/moass/lib/widgets/to_do_list.dart
+++ Frontend/moass/lib/widgets/to_do_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:moass/widgets/check_box.dart';
 
 class ToDoList extends StatefulWidget {
@@ -14,6 +15,20 @@ class ToDoList extends StatefulWidget {
 class _ToDoListState extends State<ToDoList> {
   List<String> todoList = <String>['현욱이 괴롭히기', '끝장나게 잠 자기'];
 
+  bool isAddInputAvailable = false;
+
+  void tapAddToDo() {
+    setState(() {
+      isAddInputAvailable = true;
+    });
+  }
+
+  void completeAddToDo() {
+    setState(() {
+      isAddInputAvailable = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -25,16 +40,55 @@ class _ToDoListState extends State<ToDoList> {
         child: Column(
           children: [
             for (var todo in todoList) CheckboxWidget(text: todo),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.add_box_outlined),
-                  Text('할 일 추가'),
-                ],
-              ),
-            )
+            Container(
+              child: isAddInputAvailable
+                  ? SizedBox(
+                      height: 40,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration.collapsed(
+                              border: UnderlineInputBorder(),
+                              hintText: '오늘 할 일은 뭔가요?'),
+                          textInputAction: TextInputAction.done,
+                        ),
+                      ),
+                    )
+                  : null,
+            ),
+            isAddInputAvailable
+                ? GestureDetector(
+                    onTap: completeAddToDo,
+                    child: Container(
+                      decoration: const BoxDecoration(color: Color(0xFF3DB887)),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.check_circle,
+                              color: Color(0xFF3DB887),
+                            ),
+                            Text('완료!'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: tapAddToDo,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.add_box_outlined),
+                          Text('할 일 추가'),
+                        ],
+                      ),
+                    ),
+                  )
           ],
         ),
       ),
