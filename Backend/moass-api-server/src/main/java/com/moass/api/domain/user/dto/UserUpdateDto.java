d631b7af9fe1f5035f919d440d87d73ffa3ce5a4
www-jong <weon1009@gmail.com>
2024-04-26 16:46:47 +0900

    Feat: 유저정보 수정 기능

---
+++ Backend/moass-api-server/src/main/java/com/moass/api/domain/user/dto/UserUpdateDto.java
@@ -0,0 +1,28 @@
+package com.moass.api.domain.user.dto;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
+@Data
+public class UserUpdateDto {
+
+    private String userId;
+
+    private String userEmail;
+
+    private Integer statusId;
+
+    private String password;
+
+    private String profileImg;
+
+    private String backgroundImg;
+
+    private Integer rayout;
+
+    private Integer connectFlag;
+
+    private String  positionName;
+
+}
