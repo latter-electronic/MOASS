da865c369a808f20b8bb5b649abbe45b3d184075
www-jong <weon1009@gmail.com>
2024-04-28 02:59:31 +0900

    Add: 전역예외 추가

--- Backend/moass-api-server/src/main/java/com/moass/api/global/handler/GlobalExceptionHandler.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/global/handler/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
@@ -37,6 +39,36 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public Mono<ResponseEntity<ApiResponse>> NullPointerException(NullPointerException e) {
 		log.warn("dataAccessException: ", e);
 		return ApiResponse.error(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(IllegalAccessException.class)
+	public Mono<ResponseEntity<ApiResponse>> IllegalAccessException(IllegalAccessException e) {
+		log.warn("dataAccessException: ", e);
+		return ApiResponse.error(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public Mono<ResponseEntity<ApiResponse>> IllegalArgumentException(IllegalArgumentException e) {
+		log.warn("dataAccessException: ", e);
+		return ApiResponse.error(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public Mono<ResponseEntity<ApiResponse>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+		log.warn("HttpMessageNotReadableException: ", e);
+		return ApiResponse.error("Invalid request content", HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Mono<ResponseEntity<ApiResponse>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		log.warn("MethodArgumentNotValidException: ", e);
+		return ApiResponse.error("Invalid request content", HttpStatus.BAD_REQUEST);
+	}
+
+	// 기타 예외처리
+	@ExceptionHandler(Exception.class)
+	public Mono<ResponseEntity<ApiResponse>> handleException(CustomException e) {
+		log.error("Exception: ", e);
+		return ApiResponse.error("Internal server error", e.getStatus());
 	}
 	/**
 	private ApiResponse<Object> makeErrorResponse(ErrorCode errorCode) {
