be47017b8f3a6f9b04c60ea4e3f71974aa6eafbc
RosaDamascena <hsj990604@gmail.com>
2024-04-28 23:40:36 +0900

    Refactor: AuthStore 구조 수정
    accessToken, refreshToken 저장

--- Embedded/electron-app/src/renderer/src/stores/AuthStore.js
+++ Embedded/electron-app/src/renderer/src/stores/AuthStore.js
@@ -1,11 +1,27 @@
+// src/store/useAuthStore.js
+
 /* 인증 관련 상태를 관리
-   로그인, 로그아웃, 사용자 인증 상태 확인 등 인증과 관련된 상태 관리*/
-import { create } from 'zustand';
+로그인, 로그아웃, 사용자 인증 상태 확인 등 인증과 관련된 상태 관리*/
+import create from 'zustand'
 
 const useAuthStore = create(set => ({
-  isAuthenticated: false,
-  login: () => set({ isAuthenticated: true }),
-  logout: () => set({ isAuthenticated: false })
+  isAuthenticated: false,  // 로그인 상태
+  accessToken: null,  // 액세스 토큰
+  refreshToken: null, // 리프레시 토큰
+
+  // 로그인 상태 설정
+  login: (accessToken, refreshToken) => set({
+    isAuthenticated: true,
+    accessToken,
+    refreshToken
+  }),
+
+  // 로그아웃 상태 설정
+  logout: () => set({
+    isAuthenticated: false,
+    accessToken: null,
+    refreshToken: null
+  })
 }));
 
 export default useAuthStore;
