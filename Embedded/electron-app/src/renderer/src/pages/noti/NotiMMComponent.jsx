232b1d04431b5b444cfbafa25e2e7daf8917f069
서지수 <diduedidue@naver.com>
2024-04-29 15:53:41 +0900

    Style: MM 컴포넌트 수정

--- Embedded/electron-app/src/renderer/src/pages/noti/NotiMMComponent.jsx
+++ Embedded/electron-app/src/renderer/src/pages/noti/NotiMMComponent.jsx
@@ -3,27 +3,28 @@ import mattermostIcon from './testImg/mattermost-icon.svg';
 
 export default function NotiMMComponent() {
   return (
-    <div className="flex items-center p-3 bg-mmBlue rounded-lg overflow-hidden shadow-md">
-        
+    <div className="flex items-center p-3 bg-mmBlue/60 rounded-lg overflow-hidden shadow-md">
+
       {/* 팀 아이콘 */}
-      <div className="flex-shrink-0 mr-3">
+      <div className="flex-shrink-0 mr-3 bg-white rounded-md">
         <img src={teamIcon} alt="Team Icon" className="w-10 h-10 rounded-full" />
       </div>
 
       {/* 알림 내용 */}
       <div className="flex-grow">
         <div className="flex items-baseline">
-          <span className="text-sm font-medium text-white mr-2">긴급시안함</span>
+          <span className="text-sm font-medium text-white mr-2">공지사항</span>
           <span className="text-xs text-gray-400">오전 9:00</span>
         </div>
-        <div className="text-blue-300 text-sm">
-          @hosun.hwang: 개발자님 코드를 검사했습니다.
+        <div className="text-white text-sm">
+            <span className="font-medium">@hosun.hwang: </span>
+            <span className="font-light">가 메세지를 게시했습니다.</span>
         </div>
       </div>
 
       {/* Mattermost 로고 */}
       <div className="flex-shrink-0 ml-3">
-        <img src={mattermostIcon} alt="Mattermost Icon" className="w-6 h-6" />
+        <img src={mattermostIcon} alt="Mattermost Icon" className="w-6 h-6 opacity-20" />
       </div>
 
     </div>
