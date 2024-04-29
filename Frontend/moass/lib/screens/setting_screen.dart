a9933a1fc1f33c66f8a8743ab927fd2bda2b08ab
손종민 <kain9101@naver.com>
2024-04-29 17:26:31 +0900

    Feat: 연동 계정 관리 스크린 생성 및 라우팅 연결

--- Frontend/moass/lib/screens/setting_screen.dart
+++ Frontend/moass/lib/screens/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moass/screens/setting_related_account.dart';
 import 'package:moass/widgets/category_text.dart';
 import 'package:moass/widgets/top_bar.dart';
 
@@ -107,18 +108,58 @@ class SettingScreen extends StatelessWidget {
               decoration: BoxDecoration(
                   border: Border(
                       bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: CategoryText(text: '연동 계정 관리'),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          // fullscreenDialog: true,
+                          builder: (context) =>
+                              const SettingRelatedAccountScreen(),
+                        ),
+                      );
+                    },
+                    child: const CategoryText(text: '연동 계정 관리')),
               )),
           Container(
               width: double.infinity,
               decoration: BoxDecoration(
                   border: Border(
                       bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: CategoryText(text: '로그아웃'),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: GestureDetector(
+                    onTap: () => showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('기기 연결 해제'),
+                            content: SizedBox(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Image.asset(
+                                    'assets/img/crying_mozzi.png',
+                                    height: 200,
+                                  ),
+                                  const Text('앱에서 로그아웃 하시겠어요?'),
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, '취소'),
+                                child: const Text('취소'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, '확인'),
+                                child: const Text('확인'),
+                              ),
+                            ],
+                          ),
+                        ),
+                    child: const CategoryText(text: '로그아웃')),
               )),
         ],
       ),
