2d7ac8e561d3a70c22525cd769613d947987bf17
이동호 <ghehd1125@gmail.com>
2024-04-30 01:44:10 +0900

    Feat: S3 이미지 업로드 구현

--- Backend/moass-api-server/src/main/java/com/moass/api/domain/file/controller/FileController.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/domain/file/controller/FileController.java
@@ -1,15 +1,76 @@
 package com.moass.api.domain.file.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.moass.api.domain.file.dto.UploadResult;
+import com.moass.api.global.config.S3ClientConfigurationProperties;
+import com.moass.api.global.response.ApiResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import software.amazon.awssdk.core.SdkResponse;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/upload")
 public class FileController {
     private final S3AsyncClient s3client;
+    private final S3ClientConfigurationProperties s3config;
 
-    public FileController(S3AsyncClient s3client) {
+    public FileController(S3AsyncClient s3client, S3ClientConfigurationProperties s3config) {
         this.s3client = s3client;
+        this.s3config = s3config;
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<UploadResult>> uploadHandler(@RequestHeader HttpHeaders headers, @RequestBody Flux<ByteBuffer> body) {
+        long length = headers.getContentLength();
+        if (length < 0) {
+            throw new UploadFailedException(HttpStatus.BAD_REQUEST.value(), Optional.of("required header missing: Content-Length"));
+        }
+
+        String fileKey = UUID.randomUUID().toString();
+        Map<String, String> metadata = new HashMap<String, String>();
+        MediaType mediaType = headers.getContentType();
+
+        if (mediaType == null) {
+            mediaType = MediaType.APPLICATION_OCTET_STREAM;
+        }
+
+        CompletableFuture<PutObjectResponse> future = s3client
+                .putObject(PutObjectRequest.builder()
+                                .bucket(s3config.getBucket())
+                                .contentLength(length)
+                                .key(fileKey)
+                                .contentType(mediaType.toString())
+                                .metadata(metadata)
+                                .build(),
+                        AsyncRequestBody.fromPublisher(body));
+
+        return Mono.fromFuture(future)
+                .map((response) -> {
+                    checkResult(response);
+                    return ResponseEntity
+                            .status(HttpStatus.CREATED)
+                            .body(new UploadResult(HttpStatus.CREATED, new String[] {fileKey}));
+                });
+    }
+
+    private static void checkResult(SdkResponse result) {
+        if (result.sdkHttpResponse() == null || !result.sdkHttpResponse().isSuccessful()) {
+            throw new UploadFailedException(result);
+        }
     }
 }
