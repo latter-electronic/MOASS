5629489d0f134e507a3a9ded56de975c1935cad8
www-jong <weon1009@gmail.com>
2024-04-30 15:53:02 +0900

    Add: 레포지토리 작성

--- Backend/moass-api-server/src/main/java/com/moass/api/domain/user/repository/ClassRepository.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/domain/user/repository/ClassRepository.java
@@ -15,10 +15,10 @@ public interface ClassRepository extends ReactiveCrudRepository<Class, Integer> 
 
     Mono<Class> findByClassCode(String classCode);
 
-    @Query("SELECT l.location_code, l.location_name, c.class_code, t.team_code, t.team_name, u.user_id, u.user_email, su.user_name " +
-            "u.position_name, u.status_id, u.profile_img, su.job_code, u.connect_flag" +
+    @Query("SELECT l.location_code, l.location_name, c.class_code, t.team_code, t.team_name, u.user_id, u.user_email, su.user_name, " +
+            "u.position_name, u.status_id, u.profile_img, su.job_code, u.connect_flag " +
             "FROM Class c " +
-            "JOIN Location l ON c.location_code = l.location_code" +
+            "JOIN Location l ON c.location_code = l.location_code " +
             "JOIN Team t ON c.class_code = t.class_code " +
             "JOIN SsafyUser su ON t.team_code = su.team_code " +
             "JOIN User u ON su.user_id = u.user_id " +
--- Backend/moass-api-server/src/main/java/com/moass/api/domain/user/repository/UserRepository.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/domain/user/repository/UserRepository.java
@@ -5,7 +5,6 @@ import com.moass.api.domain.user.entity.UserDetail;
 import com.moass.api.domain.user.entity.UserSearchDetail;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
@@ -16,24 +15,15 @@ public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
             "WHERE u.user_email = :userEmail")
     Mono<UserDetail> findUserDetailByUserEmail(String userEmail);
 
-    @Query("SELECT u.user_id, u.user_email, u.status_id, u.password, u.profile_img, u.background_img, " +
-            "u.layout, u.connect_flag, s.card_serial_id, s.job_code, s.team_code, s.user_name, u.position_name " +
-            "FROM User u "+
-            "INNER JOIN SsafyUser s ON u.user_id = s.user_id " +
-            "WHERE s.team_code = :teamCode " +
-            "ORDER BY s.user_name ASC")
-    Flux<UserDetail> findAllTeamUserByTeamCode(String teamCode);
-
-    @Query("SELECT c.location_code, l.location_name, c.class_code, t.team_code, t.team_name, " +
-            "s.user_id, u.user_email, s.user_name, u.position_name, u.status_id, u.profile_img, s.job_code, u.connect_flag " +
+    @Query("SELECT l.location_code, l.location_name, c.class_code, t.team_code, t.team_name, " +
+            "u.user_id, u.user_email, su.user_name, u.position_name, u.status_id, u.profile_img, su.job_code, u.connect_flag " +
             "FROM User u " +
-            "INNER JOIN SsafyUser s ON u.user_id = s.user_id " +
-            "INNER JOIN Team t ON s.team_code = t.team_code " +
-            "INNER JOIN Class c ON t.class_code = c.class_code " +
+            "INNER JOIN SsafyUser su ON u.user_id = su.user_id " +
+            "INNER JOIN Team t ON t.team_code = su.team_code " +
+            "INNER JOIN Class c ON c.class_code = t.class_code " +
             "INNER JOIN Location l ON l.location_code = c.location_code " +
-            "WHERE s.team_code = :teamCode " +
-            "ORDER BY s.user_name ASC")
-    Flux<UserSearchDetail> findAllTeamUserByTeamCode2(String teamCode);
+            "WHERE u.user_email = :userEmail")
+    Mono<UserSearchDetail> findUserSearchDetailByUserEmail(String userEmail);
 
     Mono<User> findByUserEmail(String userEmail);
 
