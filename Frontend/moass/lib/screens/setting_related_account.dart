22d6babd65bd07cb04d6488f8fdef540c2efc224
손종민 <kain9101@naver.com>
2024-04-30 11:47:15 +0900

    Feat: 연동 계정 관리 페이지 껍데기 구현

--- Frontend/moass/lib/screens/setting_related_account.dart
+++ Frontend/moass/lib/screens/setting_related_account.dart
@@ -8,108 +8,15 @@ class SettingRelatedAccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: const TopBar(
+    return const Scaffold(
+        appBar: TopBar(
           title: '연동 계정 관리',
           icon: Icons.settings,
         ),
         body: Column(children: [
-          const SetRelatedAccount(),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                            offset: const Offset(1, 1),
-                            blurRadius: 1,
-                            color: Colors.black.withOpacity(0.5),
-                            spreadRadius: 1)
-                      ],
-                    ),
-                    child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: const Color(0xFF28427b),
-                        child: Image.asset('assets/img/logo_mattermost.png')),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      '연결된 계정이 없습니다',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'MatterMost',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                            offset: const Offset(1, 1),
-                            blurRadius: 1,
-                            color: Colors.black.withOpacity(0.5),
-                            spreadRadius: 1)
-                      ],
-                    ),
-                    child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Colors.white,
-                        child: Image.asset(
-                          'assets/img/logo_gitlab.png',
-                          width: 40,
-                        )),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      '연결된 hook이 없습니다',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'Gitlab',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
+          SetRelatedAccount(service: 'jira'),
+          SetRelatedAccount(service: 'mattermost'),
+          SetRelatedAccount(service: 'gitlab'),
         ]));
   }
 }
--- Frontend/moass/lib/widgets/set_related_account.dart
+++ Frontend/moass/lib/widgets/set_related_account.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SetRelatedAccount extends StatelessWidget {
-  const SetRelatedAccount({
-    super.key,
-  });
+class SetRelatedAccount extends StatefulWidget {
+  final String service;
+  const SetRelatedAccount({super.key, required this.service});
+
+  @override
+  State<SetRelatedAccount> createState() => _SetRelatedAccountState();
+}
+
+class _SetRelatedAccountState extends State<SetRelatedAccount> {
+  bool isOpenedButtonWidget = false;
+
+  openButtonWidget() {
+    setState(() {
+      isOpenedButtonWidget = !isOpenedButtonWidget;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,44 +25,84 @@ class SetRelatedAccount extends StatelessWidget {
       decoration: BoxDecoration(
           border:
               Border(bottom: BorderSide(color: Colors.grey.withOpacity(0.5)))),
-      child: Row(
+      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                      offset: const Offset(1, 1),
-                      blurRadius: 1,
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 1)
-                ],
-              ),
-              child: CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Image.asset('assets/img/logo_jira.png')),
+          GestureDetector(
+            onTap: () {
+              openButtonWidget();
+            },
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: const Offset(1, 1),
+                            blurRadius: 1,
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 1)
+                      ],
+                    ),
+                    child: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: widget.service == 'mattermost'
+                            ? const Color(0xFF28427b)
+                            : Colors.white,
+                        child: Image.asset(
+                          'assets/img/logo_${widget.service}.png',
+                          width: 35,
+                          height: 35,
+                        )),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    widget.service == 'gitlab'
+                        ? const Text(
+                            '연결된 hook URL이 없습니다',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )
+                        : const Text(
+                            '연결된 계정이 없습니다',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                    Text(
+                      widget.service,
+                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                '연결된 계정이 없습니다',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                'Jira',
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              )
-            ],
-          ),
+          isOpenedButtonWidget
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(),
+                        child: widget.service == 'gitlab'
+                            ? const Text('URL 등록')
+                            : const Text('계정 연동'),
+                      ),
+                    )
+                  ],
+                )
+              : const Row()
         ],
       ),
     );
