a85f1316c3722bc4018c6e1cf8e749d6a659785d
jongmin Son <kain9101@naver.com>
2024-05-01 17:43:56 +0900

    Feat: 설정창 토글버튼, notification 박스 위젯

--- Frontend/moass/lib/screens/notification_screen.dart
+++ Frontend/moass/lib/screens/notification_screen.dart
@@ -6,11 +6,51 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TopBar(
-        title: '알림',
-        icon: Icons.notifications,
-      ),
-    );
+    return Scaffold(
+        appBar: const TopBar(
+          title: '알림',
+          icon: Icons.notifications,
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFF6ECEF5), width: 2.0),
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            child: Image.asset(
+                              'assets/img/noti_icon.png',
+                              width: 30,
+                            ),
+                          ),
+                          const Text(
+                            '알림',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                      Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: const Text('알림 내용'))
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
