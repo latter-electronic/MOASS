c284a74ce496ecf2d7e0d9338108028bf8728cff
서지수 <diduedidue@naver.com>
2024-05-01 21:42:54 +0900

    Feat: todo 조회 API 연결

--- Embedded/electron-app/src/renderer/src/pages/home/HomeTodoListComponent.jsx
+++ Embedded/electron-app/src/renderer/src/pages/home/HomeTodoListComponent.jsx
@@ -1,38 +1,69 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { fetchTodos } from '../../services/todoService.js'
 
 export default function HomeTodoListComponent() {
-    const [todos, setTodos] = useState([
-        { id: 1, text: 'BRENA 투어', isCompleted: false },
-        { id: 2, text: '2주차 KPT 회고', isCompleted: false },
-        { id: 3, text: '코치님한테 여쭤볼거', isCompleted: false },
-        { id: 4, text: '노트북 챙기기', isCompleted: false },
-    ]);
+    const [todos, setTodos] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const toggleTodo = (id) => {
+    useEffect(() => {
+        const loadTodos = async () => {
+            setIsLoading(true);
+            setError(null);
+            try {
+                const response = await fetchTodos();  // Todo 가져오기
+                setTodos(response.data.data.map(todo => ({
+                    todoId: todo.todoId,
+                    content: todo.content,
+                    completedFlag: todo.completedFlag,
+                    createdAt: todo.createdAt,
+                    updatedAt: todo.updatedAt,
+                    completedAt: todo.completedAt,
+                })));
+            } catch (err) {
+                setError(err.message);  // 에러
+                setTodos([
+                    { todoId: 1, userId: "1058448", content: 'BRENA 투어', completedFlag: false, createdAt: "2024-04-27T16:22:41.575", updatedAt: "2024-04-27T16:22:41.575", completedAt: null },
+                    { todoId: 2, userId: "1058448", content: '2주차 KPT 회고', completedFlag: false, createdAt: "2024-04-27T16:22:43.876", updatedAt: "2024-04-27T16:22:43.876", completedAt: null },
+                    { todoId: 3, userId: "1058448", content: '코치님한테 여쭤볼거', completedFlag: false, createdAt: "2024-04-27T16:22:46.746", updatedAt: "2024-04-27T16:22:46.746", completedAt: null },
+                    { todoId: 4, userId: "1058448", content: '노트북 챙기기', completedFlag: false, createdAt: "2024-04-27T16:23:46.746", updatedAt: "2024-04-27T16:24:46.746", completedAt: null }
+                ]);
+            } finally {
+                setIsLoading(false);  // 로딩 상태 해제
+            }
+        };
+
+        loadTodos();
+    }, []);
+
+    const toggleTodo = (todoId) => {
         setTodos(
             todos.map((todo) =>
-                todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+                todo.todoId === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
             )
         );
     };
 
+    if (isLoading) return <div>로딩중...</div>;
+    // if (error) return <div>To do List를 가져오는데 실패했어요😥{error}</div>;  // 개발 완료후 바꾸기
+
     return (
-        <div className="bg-white/5 p-6 rounded-lg max-w-sm mt-5">
+        <div className="bg-white/5 p-6 rounded-lg w-72 mt-5 h-60 scrollbar-hide overflow-y-auto">
             <h1 className="text-white text-2xl font-bold mb-4">To do List ✨</h1>
             <ul>
                 {todos.map((todo) => (
-                    <li key={todo.id} className="flex items-center mb-2 mr-12 text-xl">
+                    <li key={todo.todoId} className="flex items-center mb-2 text-xl">
                         <input
-                            id={`todo-${todo.id}`}
+                            id={`todo-${todo.todoId}`}
                             type="checkbox"
                             checked={todo.isCompleted}
-                            onChange={() => toggleTodo(todo.id)}
+                            onChange={() => toggleTodo(todo.todoId)}
                             className="form-checkbox h-5 w-5 text-blue-600"
                         />
-                        <label htmlFor={`todo-${todo.id}`} className="ml-2 text-white font-light cursor-pointer">
+                        <label htmlFor={`todo-${todo.todoId}`} className="ml-2 text-white font-light cursor-pointer">
                             <span className={todo.isCompleted ? 'line-through' : ''}>
-                                {todo.text}
+                                {todo.content}
                             </span>
                         </label>
                     </li>
