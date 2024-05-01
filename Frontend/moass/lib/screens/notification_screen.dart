c0806521fc18880cc4f4ee24a60b93ca3b97e88d
jongmin Son <kain9101@naver.com>
2024-05-01 22:03:23 +0900

    Fix: git conflict 해결

--- Frontend/moass/lib/screens/notification_screen.dart
+++ Frontend/moass/lib/screens/notification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moass/widgets/noti_widget.dart';
 import 'package:moass/widgets/top_bar.dart';
 
 class NotificationScreen extends StatelessWidget {
@@ -6,51 +7,13 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: const TopBar(
+    return const Scaffold(
+        appBar: TopBar(
           title: '알림',
           icon: Icons.notifications,
         ),
         body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    border:
-                        Border.all(color: const Color(0xFF6ECEF5), width: 2.0),
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 10),
-                            child: Image.asset(
-                              'assets/img/noti_icon.png',
-                              width: 30,
-                            ),
-                          ),
-                          const Text(
-                            '알림',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w600),
-                          ),
-                        ],
-                      ),
-                      Container(
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          child: const Text('알림 내용'))
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+          children: [NotiWidget()],
         ));
   }
 }
