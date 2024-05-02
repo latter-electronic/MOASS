86e880be2a1b2e86b588d262658cd64236c6ba6e
dongho <ghehd1125@gmail.com>
2024-05-02 15:43:19 +0900

    Feat: WS 초기 환경

---
+++ Backend/moass-ws-server/src/main/java/com/moass/ws/MoassWsServerApplication.java
@@ -0,0 +1,13 @@
+package com.moass.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoassWsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoassWsServerApplication.class, args);
+    }
+
+}
