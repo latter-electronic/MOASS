f381a4e0f9b0173a0f9aa001a4e3e3dc2464df72
서지수 <diduedidue@naver.com>
2024-05-03 11:54:13 +0900

    Feat: 로그인 테스트 구현 및 api 연결

--- Embedded/electron-app/src/renderer/src/pages/common/TestLoginPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/common/TestLoginPage.jsx
@@ -1,7 +1,57 @@
-export default function TestLoginPage(){
-    return(
-        <div>
-            로그인테스트
+import React, { useState } from 'react';
+import { deviceLogin } from '../../services/deviceService.js';
+import AuthStore from '../../stores/AuthStore.js';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function TestLoginPage() {
+    const navigate = useNavigate();
+
+    const [deviceId, setDeviceId] = useState('');
+    const [cardSerialId, setCardSerialId] = useState('');
+    const { login, accessToken, refreshToken } = AuthStore(state => ({
+        login: state.login,
+        accessToken: state.accessToken,
+        refreshToken: state.refreshToken
+    }));
+
+
+    const handleLogin = async () => {
+        try {
+            const response = await deviceLogin({ deviceId, cardSerialId });
+            const { accessToken, refreshToken } = response.data.data;
+            login(accessToken, refreshToken);
+            alert(`로그인 성공: \nAccessToken: ${accessToken}\nRefreshToken: ${refreshToken}`);
+            navigate(`/`);
+        } catch (error) {
+            alert(`로그인 실패: ${error.response?.data?.message}`);
+        }
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen  w-screen">
+            <div className="p-8 bg-white shadow-md rounded">
+                <h1 className="text-2xl font-bold text-center mb-6 text-black">로그인테스트</h1>
+                <input
+                    className="w-full p-2 mb-4 text-sm text-gray-700 bg-gray-100 rounded border border-gray-300"
+                    type="text"
+                    placeholder="기기 ID"
+                    value={deviceId}
+                    onChange={(e) => setDeviceId(e.target.value)}
+                />
+                <input
+                    className="w-full p-2 mb-4 text-sm text-gray-700 bg-gray-100 rounded border border-gray-300"
+                    type="text"
+                    placeholder="카드 시리얼 ID"
+                    value={cardSerialId}
+                    onChange={(e) => setCardSerialId(e.target.value)}
+                />
+                <button 
+                    className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                    onClick={handleLogin}>
+                    로그인
+                </button>
+            </div>
         </div>
     );
 }
--- Embedded/electron-app/src/renderer/src/stores/AuthStore.js
+++ Embedded/electron-app/src/renderer/src/stores/AuthStore.js
@@ -4,19 +4,17 @@
 로그인, 로그아웃, 사용자 인증 상태 확인 등 인증과 관련된 상태 관리*/
 import { create } from 'zustand'
 
-const useAuthStore = create(set => ({
+const AuthStore = create(set => ({
   isAuthenticated: false,  // 로그인 상태
   accessToken: null,  // 액세스 토큰
   refreshToken: null, // 리프레시 토큰
 
-  // 로그인 상태 설정
   login: (accessToken, refreshToken) => set({
     isAuthenticated: true,
     accessToken,
     refreshToken
   }),
 
-  // 로그아웃 상태 설정
   logout: () => set({
     isAuthenticated: false,
     accessToken: null,
@@ -24,4 +22,4 @@ const useAuthStore = create(set => ({
   })
 }));
 
-export default useAuthStore;
+export default AuthStore;
