8bf2337f554952fabf4031c0b98ccaeb30349880
서지수 <diduedidue@naver.com>
2024-05-05 22:57:17 +0900

    Feat: issue search 조회결과 json 파일 자동 저장

--- Embedded/electron-app/src/renderer/src/pages/jira/test/issueSearch.js
+++ Embedded/electron-app/src/renderer/src/pages/jira/test/issueSearch.js
@@ -1,20 +1,28 @@
 (async () => {
     const { default: fetch } = await import('node-fetch');
+    const { writeFile } = await import('fs/promises');
+
     const jqlQuery = encodeURIComponent(`project = 'S10P31E203' AND sprint IN closedSprints() AND sprint NOT IN futureSprints() ORDER BY sprint DESC`);
-    // 원래는 project = 'S10P31E203' AND sprint IN openSprints() 인데 지금 열린 스프린트가 없어서 제일 최신 스프린트로 불러옴
+    const url = `https://ssafy.atlassian.net/rest/api/3/search?jql=${jqlQuery}&maxResults=10`;
   
-    fetch(`https://ssafy.atlassian.net/rest/api/3/search?jql=${jqlQuery}&maxResults=10`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Basic ${Buffer.from('diduedidue@naver.com:ATATT3xFfGF0CQg6pdozVLzR_9t-KbjN0eVvOCFboUHE9I7HGTj9ChXe7VV-HtdlfYFkcXFZ6QMELuo0uhFvnNNWk6alPHKA2vUz2FnHtr2dRUm-8q2_IzH8NWz-h_OjJpbsGRCFg42yH5p5tztaSoV6z-pzSB2PxwwnOJ390Qe0D8TENAxzlBg=A98212EB').toString('base64')}`,
-        'Accept': 'application/json'
-      }
-    })
-      .then(response => {
-        console.log(`Response: ${response.status} ${response.statusText}`);
-        return response.text();
-      })
-      .then(text => console.log(text))
-      .catch(err => console.error(err));
-  })();
-  
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${Buffer.from('diduedidue@naver.com:ATATT3xFfGF0CQg6pdozVLzR_9t-KbjN0eVvOCFboUHE9I7HGTj9ChXe7VV-HtdlfYFkcXFZ6QMELuo0uhFvnNNWk6alPHKA2vUz2FnHtr2dRUm-8q2_IzH8NWz-h_OjJpbsGRCFg42yH5p5tztaSoV6z-pzSB2PxwwnOJ390Qe0D8TENAxzlBg=A98212EB').toString('base64')}`,
+                'Accept': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const filePath = './issueSearch.json';
+        await writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+        console.log('Data saved to test.json');
+    } catch (err) {
+        console.error('Error:', err);
+    }
+})();
