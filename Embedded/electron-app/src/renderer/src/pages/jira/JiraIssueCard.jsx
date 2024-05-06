2e4e8a507b6de3aae312505647c40d0ba80c2438
서지수 <diduedidue@naver.com>
2024-05-07 03:04:15 +0900

    Feat: 지라 이슈 불러오기 구현

--- Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
+++ Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
@@ -1,16 +1,12 @@
 import IssueIcon from '../../assets/images/jira/jiraIssueIcon.svg'
-import profileImg from '../../assets/images/jira/jiraProfileImg1.png'
-import highestIcon from '../../assets/images/jira/p-highest.svg'
-import highIcon from '../../assets/images/jira/p-high.svg'
-import mediumIcon from '../../assets/images/jira/p-medium.svg'
-import lowIcon from '../../assets/images/jira/p-low.svg'
-import lowestIcon from '../../assets/images/jira/p-low.svg'
 
-export default function JiraIssueCard() {
+export default function JiraIssueCard({ issue }) {
+    const { summary, creator, priority, status } = issue.fields;
+
     return (
         <div className="flex flex-col mt-2 bg-white rounded-lg shadow p-3 px-5 gap-1 text-base">
 
-            <div className="font-medium text-lg text-gray-700">Electron 개발 초기 환경 테스트</div>
+            <div className="font-medium text-lg text-gray-700">{summary}</div>
             
             <div className="flex justify-between items-center">
                 <div className="flex items-center gap-2">
@@ -19,8 +15,8 @@ export default function JiraIssueCard() {
                 </div>
                 <div className="flex items-center gap-2 mt-1">
                     <span className="bg-gray-200 rounded-full text-sm font-semibold px-2 py-1 text-gray-700">2</span>
-                    <img src={mediumIcon} alt="중간중요도" className="size-6"/>
-                    <img src={profileImg} alt="지라프로필1" className="rounded-full size-10" />
+                    <img src={priority.iconUrl} alt="중요도 아이콘" className="size-6"/>
+                    <img src={creator.avatarUrls["48x48"]} alt="지라프로필1" className="rounded-full size-10" />
                 </div>
             </div>
 
