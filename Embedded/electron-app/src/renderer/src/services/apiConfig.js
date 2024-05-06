170e946cec2c2ab30b40e2b955cbc30f5b31e2e8
서지수 <diduedidue@naver.com>
2024-05-07 02:00:54 +0900

    Feat: jira API axios 등록

--- Embedded/electron-app/src/renderer/src/services/apiConfig.js
+++ Embedded/electron-app/src/renderer/src/services/apiConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const MOASS_API_URL = import.meta.env.VITE_MOASS_API_URL;
+const JIRA_API_URL = import.meta.env.VITE_JIRA_API_URL;
+const JIRA_API_EMAIL = import.meta.env.VITE_JIRA_API_EMAIL;
+const JIRA_API_TOKEN = import.meta.env.VITE_JIRA_API_TOKEN;
 
 export function moassApiAxios() {
     return axios.create({
@@ -11,3 +14,14 @@ export function moassApiAxios() {
         withCredentials: true
     });
 }
+
+export function jiraApiAxios() {
+    const token = Buffer.from(`${JIRA_API_EMAIL}:${JIRA_API_TOKEN}`).toString('base64');
+    return axios.create({
+      baseURL: JIRA_API_URL,
+      headers: {
+        'Authorization': `Basic ${token}`,
+        'Accept': 'application/json'
+      }
+    });
+  }
---
+++ Embedded/electron-app/src/renderer/src/services/jiraService.js
@@ -0,0 +1,52 @@
+// jiraService.js
+import { jiraApiAxios } from './apiConfig';
+
+const axios = jiraApiAxios();
+const prefix = 'rest/api/3';
+
+/**
+ * 현재 열려있는 스프린트의 이슈들 검색
+ * 
+ * @returns {Promise} 이슈 데이터
+ */
+export const fetchCurrentSprintIssues = async () => {
+    const fields = 'creator,summary,priority,status';
+    const jqlQuery = encodeURIComponent("project = 'S10P31E203' AND sprint IN openSprints()");
+    const url = `${prefix}/search?jql=${jqlQuery}&fields=${fields}&maxResults=10`;
+
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Error fetching current sprint issues:', error);
+        });
+};
+
+/**
+ * 최근 마감된 스프린트의 이슈들 검색
+ * 
+ * @returns {Promise} 이슈 데이터
+ */
+export const fetchRecentClosedSprintIssues = async () => {
+    const fields = 'creator,summary,priority,status';
+    const jqlQuery = encodeURIComponent("project = 'S10P31E203' AND sprint IN closedSprints() AND sprint NOT IN futureSprints() ORDER BY sprint DESC");
+    const url = `${prefix}/search?jql=${jqlQuery}&fields=${fields}&maxResults=10`;
+
+    return axios.get(url)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Error fetching recent closed sprint issues:', error);
+        });
+};
+
+/**
+ * 모든 Jira 프로젝트 검색
+ * 
+ * @returns {Promise} 프로젝트 목록
+ */
+export const fetchProjects = () => {
+    return axios.get(`${prefix}/project/search`)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Error fetching projects:', error);
+        });
+};
