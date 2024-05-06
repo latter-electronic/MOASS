c3c53b4716848c086e4122cb9dfdcad2e39bc842
서지수 <diduedidue@naver.com>
2024-05-07 05:50:27 +0900

    Feat: 스토리포인트 반영

--- Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
+++ Embedded/electron-app/src/renderer/src/pages/jira/JiraIssueCard.jsx
@@ -2,7 +2,7 @@ import IssueIcon from '../../assets/images/jira/jiraIssueIcon.svg'
 import { getEpicColorClasses } from '../../utils/colorUtils.js';
 
 export default function JiraIssueCard({ issue }) {
-    const { summary, creator, priority, status, parent } = issue.fields;
+    const { summary, creator, priority, customfield_10031, parent } = issue.fields;
     const { text, bg } = getEpicColorClasses(parent.fields.summary);
 
     return (
@@ -16,9 +16,9 @@ export default function JiraIssueCard({ issue }) {
                     <span className={`${text} ${bg} px-2 py-1 rounded font-semibold`}>{parent.fields.summary}</span>
                 </div>
                 <div className="flex items-center gap-2 mt-1">
-                    <span className="bg-gray-200 rounded-full text-sm font-semibold px-2 py-1 text-gray-700">2</span>
+                    <span className="bg-gray-200 rounded-full text-sm font-semibold px-2 py-1 text-gray-700">{customfield_10031}</span>
                     <img src={priority.iconUrl} alt="중요도 아이콘" className="size-6"/>
-                    <img src={creator.avatarUrls["48x48"]} alt="지라프로필1" className="rounded-full size-10" />
+                    <img src={creator.avatarUrls["48x48"]} alt="지라프로필" className="rounded-full size-10" />
                 </div>
             </div>
 
