083a0af8aff04adb7876aa1d48937b100f707601
서지수 <diduedidue@naver.com>
2024-05-07 11:33:49 +0900

    Feat: 현재 진행중 스프린트값으로 변경

--- Embedded/electron-app/src/renderer/src/pages/jira/test/jiraAPItest.mjs
+++ Embedded/electron-app/src/renderer/src/pages/jira/test/jiraAPItest.mjs
@@ -5,8 +5,8 @@ const saveIssuesToFile = async () => {
     try {
         // jiraService 모듈을 동적으로 불러옵니다.
         const jiraService = await import('../../../services/jiraService.js');
-        const issues = await jiraService.fetchRecentClosedSprintIssues();
-        const filePath = './recentClosedSprintIssues.json';
+        const issues = await jiraService.fetchCurrentSprintIssues();
+        const filePath = './CurrentSprintAll.json';
         await fs.writeFile(filePath, JSON.stringify(issues, null, 2), 'utf8');
         console.log('Issues saved to recentClosedSprintIssues.json');
     } catch (error) {
