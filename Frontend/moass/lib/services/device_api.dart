88a82a4944b07816647393174c19b0843b9ff225
손종민 <kain9101@naver.com>
2024-05-09 10:29:32 +0900

    Init: device API

---
+++ Frontend/moass/lib/services/device_api.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class DeviceApi {
+  final Dio dio;
+  final FlutterSecureStorage storage;
+  static const String baseUrl = 'https://k10e203.p.ssafy.io';
+
+  // dio와 storage에 담긴 데이터 활용하겠다 선언
+  DeviceApi({required this.dio, required this.storage});
+
+  // Future<MyProfile?> fetchUserProfile() async {
+  //   try {
+  //     String? accessToken = await storage.read(key: 'accessToken');
+  //     if (accessToken == null) {
+  //       print('No access token available');
+  //       return null;
+  //     }
+  //     print(accessToken);
+  //     // API요청, 헤더에 토큰 넣기
+  //     final response = await dio.get(
+  //       '$baseUrl/api/user',
+  //       options: Options(headers: {'Authorization': 'Bearer $accessToken'}),
+  //     );
+
+  //     if (response.statusCode == 200) {
+  //       return MyProfile.fromJson(response.data['data']);
+  //     } else {
+  //       print('Failed to load user profile');
+  //       return null;
+  //     }
+  //   } on DioException catch (e) {
+  //     print('Error fetching user profile: ${e.message}');
+  //     return null;
+  //   }
+  // }
+}
