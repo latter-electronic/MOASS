73bf84c0b9d49f0f442222f1d65b619d9841c9a7
RosaDamascena <hsj990604@gmail.com>
2024-05-09 14:18:21 +0900

    Feat: 디바이스 2개 화면 띄우기

--- Embedded/electron-app/src/main/index.js
+++ Embedded/electron-app/src/main/index.js
@@ -1,19 +1,25 @@
 // main/index.js
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, screen } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import { spawn } from 'child_process';
 import readline from 'readline';
 import icon from '../../resources/icon.png?asset'
 
-let pythonProcess = null;
+let mainWindow = null
+let secondWindow = null
+let pythonProcess = null
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const externalDisplays = screen.getAllDisplays().filter((display) => display.id !== primaryDisplay.id)
+  console.log('externalDisplay:', externalDisplays)
+  // 메인 윈도우 설정
+  mainWindow = new BrowserWindow({
     title: 'MOASS',
     // width: 1600,
     // height: 600,
-    fullscreen: true, // 전체 화면 모드
+    fullscreen: process.platform === 'linux', // 전체 화면 모드
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -34,10 +40,18 @@ function createWindow() {
     return { action: 'deny' }
   })
 
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  // if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+  //   mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  // } else {
+  //   mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  // }
+
+  const mainWindowURL = is.dev ? process.env['ELECTRON_RENDERER_URL'] : `file://${join(__dirname, '../renderer/index.html')}`;
+  mainWindow.loadURL(mainWindowURL);
+
+  // 라즈베리파이에서만 추가 디스플레이 처리
+  if (process.platform === 'linux' && externalDisplays.length > 0) {
+    createSecondWindow(externalDisplays[0])
   }
 
   // const pythonTest = spawn('python', [join(__dirname, '../../sensors/ipc_test.py')], { encoding: 'utf8' });
@@ -67,6 +81,33 @@ function createWindow() {
 
 
   setupPythonProcess(mainWindow)
+}
+
+function createSecondWindow(display) {
+  // 두 번째 윈도우 설정
+  secondWindow = new BrowserWindow({
+    title: 'Name Space',
+    fullscreen: true, // 전체 화면 모드
+    x: display.bounds.x,
+    y: display.bounds.y,
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false,
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  console.log('xxxxxx: ', display.bounds.x)
+  console.log('yyyyyy: ', display.bounds.y)
+  console.log(join(__dirname, '../renderer/secondary.html'));
+
+  const secondWindowURL = is.dev ? `${process.env['ELECTRON_RENDERER_URL']}#/nameplate` : `file://${join(__dirname, '../renderer/index.html')}#/nameplate`;
+  secondWindow.loadURL(secondWindowURL);
+
+  // secondWindow.loadFile(join(__dirname, '../renderer/secondary.html')).then(() => {
+  //   secondWindow.webContents.send('navigate', '/nameplate');
+  // });
 }
 
 // python script 실행
@@ -129,6 +170,8 @@ app.whenReady().then(() => {
   // })
 
   createWindow()
+  const primaryDisplay = screen.getPrimaryDisplay()
+  console.log('Primary Display:', primaryDisplay)
 
   if (process.platform === 'linux') {
     ipcMain.on('login-success', (event, data) => {
