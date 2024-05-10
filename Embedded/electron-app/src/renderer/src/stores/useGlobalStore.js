380422fb4b2426b54141dbfaffc29c227d1e21ed
RosaDamascena <hsj990604@gmail.com>
2024-05-10 16:24:30 +0900

    Fix: 빠진 export문 삽입

--- Embedded/electron-app/src/renderer/src/stores/useGlobalStore.js
+++ Embedded/electron-app/src/renderer/src/stores/useGlobalStore.js
@@ -2,7 +2,7 @@
    애플리케이션 전반에서 공유되는 데이터(예: 사용자 정보, 설정, UI 상태 등)를 관리
    Zustand로 간단히 상태를 선언하고, 그걸 앱 전체에서 접근할 수 있게 할 수 있음*/
    
-   import { create } from 'zustand';
+import { create } from 'zustand';
 import { fetchUserInfo } from '../services/userService.js';
 import testUser from './test/testUser.json';
 
@@ -21,3 +21,5 @@ const useGlobalStore = create((set) => ({
   theme: 'light',
   toggleTheme: () => set((state) => ({ theme: state.theme === 'light' ? 'dark' : 'light' })),
 }));
+
+export default useGlobalStore;
