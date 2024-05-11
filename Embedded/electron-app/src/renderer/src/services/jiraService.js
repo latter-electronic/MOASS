40443a1c59d01ac41828c4d69830099aca746b2a
서지수 <diduedidue@naver.com>
2024-05-11 19:19:35 +0900

    Feat: 기존 jira API 요청 수정

--- Embedded/electron-app/src/renderer/src/services/jiraService.js
+++ Embedded/electron-app/src/renderer/src/services/jiraService.js
@@ -1,6 +1,6 @@
 import { jiraApiAxios } from './apiConfig.js';
 
-const axios = jiraApiAxios();
+const axios = moassApiAxios();
 const prefix = '';
 
 /**
@@ -9,15 +9,19 @@ const prefix = '';
  * @returns {Promise} 이슈 데이터
  */
 export const fetchCurrentSprintIssues = async () => {
-    const fields = 'creator,summary,priority,status, customfield_10031, parent';
-    const jqlQuery = encodeURIComponent("project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()");
-    const url = `${prefix}/search?jql=${jqlQuery}&fields=${fields}&maxResults=240`;
+    const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()";
+    const fields = "creator,summary,priority,status,customfield_10031,parent";
+    const maxResults = 240;
+    const url = `/api/oauth2/jira/proxy`;
 
-    return axios.get(url)
+    const payload = {
+        url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
+        method: "get"
+    };
+
+    return axios.post(url, payload)
         .then(response => response.data)
-        .catch(error => {
-            console.error('Error fetching current sprint issues:', error);
-        });
+        .catch(error => console.error('Error fetching current sprint issues:', error));
 };
 
 /**
@@ -27,15 +31,18 @@ export const fetchCurrentSprintIssues = async () => {
  */
 export const fetchRecentClosedSprintIssues = async () => {
     const fields = 'creator,summary,priority,status, customfield_10031, parent';
-    const jqlQuery = encodeURIComponent("project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN closedSprints() AND sprint NOT IN futureSprints() ORDER BY sprint DESC");
+    const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN closedSprints() AND sprint NOT IN futureSprints() ORDER BY sprint DESC";
     const maxResults = 50;  // 일단 50
-    const url = `${prefix}/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`;
+    const url = `/api/oauth2/jira/proxy`;
 
-    return axios.get(url)
+    const payload = {
+        url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
+        method: "get"
+    };
+
+    return axios.post(url, payload)
         .then(response => response.data)
-        .catch(error => {
-            console.error('Error fetching recent closed sprint issues:', error);
-        });
+        .catch(error => console.error('Error fetching recent closed sprint issues:', error));
 };
 
 /**
@@ -43,10 +50,14 @@ export const fetchRecentClosedSprintIssues = async () => {
  * 
  * @returns {Promise} 프로젝트 목록
  */
-export const fetchProjects = () => {
-    return axios.get(`${prefix}/project/search`)
+export const fetchProjects = async () => {
+    const url = `/api/oauth2/jira/proxy`;
+    const payload = {
+        url: '/rest/api/3/project/search',
+        method: "get"
+    };
+
+    return axios.post(url, payload)
         .then(response => response.data)
-        .catch(error => {
-            console.error('Error fetching projects:', error);
-        });
+        .catch(error => console.error('Error fetching projects:', error));
 };
