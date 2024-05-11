cd28f8d7459eae66b4aab4476cbdacced541b978
서지수 <diduedidue@naver.com>
2024-05-11 18:08:29 +0900

    Feat: 자동 명패 업데이트 관련

--- Embedded/electron-app/src/renderer/src/stores/AuthStore.js
+++ Embedded/electron-app/src/renderer/src/stores/AuthStore.js
@@ -10,15 +10,18 @@ const AuthStore = create((set) => ({
   cardSerialId: '',
   isCheckingAuth: true, // 로그인 상태 확인 중인지 나타내는 변수
 
-  login: (accessToken, refreshToken, deviceId, cardSerialId) =>
+  login: (accessToken, refreshToken, deviceId, cardSerialId) => {
     set({
       isAuthenticated: true,
       accessToken,
       refreshToken,
       deviceId,
       cardSerialId,
-      isCheckingAuth: false, // 확인 완료
-    }),
+      isCheckingAuth: false,
+    });
+    useGlobalStore.getState().fetchUserInfo(); // 사용자 정보 갱신
+    console.log(useGlobalStore.user)
+  },
 
   logout: () => {
     localStorage.removeItem('accessToken');
@@ -38,7 +41,7 @@ const AuthStore = create((set) => ({
 
   checkStoredAuth: async () => {
     const { fetchUserInfo } = useGlobalStore.getState();
-    set({ isCheckingAuth: true }); // 확인 시작
+    set({ isCheckingAuth: true });
     const accessToken = localStorage.getItem('accessToken');
     const refreshToken = localStorage.getItem('refreshToken');
     const deviceId = localStorage.getItem('deviceId');
@@ -56,7 +59,7 @@ const AuthStore = create((set) => ({
     } else {
       set({ isAuthenticated: false });
     }
-    set({ isCheckingAuth: false }); // 확인 완료
+    set({ isCheckingAuth: false });
   },
 }));
 
