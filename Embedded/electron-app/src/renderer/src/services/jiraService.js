a6ac6daafbd62ba2f04f513c6e95c4ab84469796
서지수 <diduedidue@naver.com>
2024-05-12 14:31:47 +0900

    Add: get Current User

--- Embedded/electron-app/src/renderer/src/services/jiraService.js
+++ Embedded/electron-app/src/renderer/src/services/jiraService.js
@@ -1,7 +1,29 @@
-import { jiraApiAxios } from './apiConfig.js';
+import { moassApiAxios } from './apiConfig.js';
 
 const axios = moassApiAxios();
-const prefix = '';
+const prefix = '/api/oauth2/jira/proxy';
+
+
+/**
+ * 현재 로그인한 사용자의 Jira 정보 조회
+ * 
+ * @returns {Promise} 사용자 정보
+ */
+export const getCurrentUser = async () => {
+    const url = `${prefix}`;
+    const payload = {
+        url: '/rest/api/3/myself',
+        method: "get"
+    };
+
+    return axios.post(url, payload)
+        .then(response => response.data)
+        .catch(error => {
+            console.error('Error fetching current user details:', error);
+            throw error;
+        });
+};
+
 
 /**
  * 현재 열려있는 스프린트의 이슈들 검색
@@ -9,10 +31,10 @@ const prefix = '';
  * @returns {Promise} 이슈 데이터
  */
 export const fetchCurrentSprintIssues = async () => {
-    const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()";
+    const url = `${prefix}`;
     const fields = "creator,summary,priority,status,customfield_10031,parent";
+    const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()";
     const maxResults = 240;
-    const url = `/api/oauth2/jira/proxy`;
 
     const payload = {
         url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
@@ -30,10 +52,10 @@ export const fetchCurrentSprintIssues = async () => {
  * @returns {Promise} 이슈 데이터
  */
 export const fetchRecentClosedSprintIssues = async () => {
+    const url = `${prefix}`;
     const fields = 'creator,summary,priority,status, customfield_10031, parent';
     const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN closedSprints() AND sprint NOT IN futureSprints() ORDER BY sprint DESC";
     const maxResults = 50;  // 일단 50
-    const url = `/api/oauth2/jira/proxy`;
 
     const payload = {
         url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
@@ -49,11 +71,16 @@ export const fetchRecentClosedSprintIssues = async () => {
  * 모든 Jira 프로젝트 검색
  * 
  * @returns {Promise} 프로젝트 목록
+ * expand: insight  통해 lastIssueUpdatedTime 키값 json 결과값에 추가하고 해당 키를 통해 desc 정렬. 마지막 결과 1개만 받아옴
  */
-export const fetchProjects = async () => {
-    const url = `/api/oauth2/jira/proxy`;
+export const getProject = async () => {
+    const url = `${prefix}`;
+    const maxResults = 1
+    const expand = 'insight'
+    const orderBy ='-lastIssueUpdatedTime'
+
     const payload = {
-        url: '/rest/api/3/project/search?orderBy=lastIssueUpdatedTime DESC&maxResults=1',
+        url: `/rest/api/3/project/search?maxResults=${maxResults}&expand=${expand}&orderBy=${orderBy}`,
         method: "get"
     };
 
