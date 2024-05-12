d607a1ffec3369f1385692e83bcf54a9ece9ea76
서지수 <diduedidue@naver.com>
2024-05-12 15:18:42 +0900

    Feat: 이슈 패치 최근 프로젝트로 하도록 수정

--- Embedded/electron-app/src/renderer/src/services/jiraService.js
+++ Embedded/electron-app/src/renderer/src/services/jiraService.js
@@ -31,19 +31,50 @@ export const getCurrentUser = async () => {
  * @returns {Promise} 이슈 데이터
  */
 export const fetchCurrentSprintIssues = async () => {
-    const url = `${prefix}`;
-    const fields = "creator,summary,priority,status,customfield_10031,parent";
-    const jqlQuery = "project = 'S10P31E203' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()";
-    const maxResults = 240;
+    try {
+        const projectData = await getProject();
+        const projectKey = projectData.values[0].key; // 가장 최근에 업데이트된 프로젝트의 키
 
+        const url = `${prefix}`;
+        const fields = "customfield_10014, summary, status, priority, assignee, customfield_10031";
+        const jqlQuery = `project = '${projectKey}' AND reporter = 'diduedidue@naver.com' AND sprint IN openSprints()`;
+        const maxResults = 240;
+
+        const payload = {
+            url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
+            method: "get"
+        };
+
+        return axios.post(url, payload)
+            .then(response => response.data)
+            .catch(error => console.error('Error fetching current sprint issues:', error));
+    } catch (error) {
+        console.error('프로젝트 키를 가져오는 데 실패했습니다:', error);
+        throw error;
+    }
+};
+
+/**
+ * 주어진 ID 또는 키로 Jira 이슈의 세부 정보 가져옴.
+ * MOASS 내부에선 epic 가져오는 용도로 쓰임
+ * 
+ * @param {string} issueIdOrKey - 검색할 이슈의 ID 또는 키
+ * @returns {Promise} 이슈 세부 정보를 나타내는 Promise 객체
+ */
+export const getIssueDetails = async (issueIdOrKey) => {
+    const url = `${prefix}`;
+    const fields = 'customfield_10011, customfield_10017'  // 10011 에픽이름, 10017 색상
     const payload = {
-        url: `/rest/api/3/search?jql=${jqlQuery}&fields=${fields}&maxResults=${maxResults}`,
+        url: `/rest/api/3/issue/${issueIdOrKey}?fields=${fields}`,
         method: "get"
     };
 
     return axios.post(url, payload)
         .then(response => response.data)
-        .catch(error => console.error('Error fetching current sprint issues:', error));
+        .catch(error => {
+            console.error(`이슈 ${issueIdOrKey}의 세부 정보를 가져오는 중 오류 발생:`, error);
+            throw error;
+        });
 };
 
 /**
