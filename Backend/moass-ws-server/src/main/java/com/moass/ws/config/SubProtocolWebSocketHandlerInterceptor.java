306dbf417868cf217274891ca62a324588d96305
이동호 <ghehd1125@gmail.com>
2024-05-13 02:51:45 +0900

    Fix: 메시지 크기 수정

---
+++ Backend/moass-ws-server/src/main/java/com/moass/ws/config/SubProtocolWebSocketHandlerInterceptor.java
@@ -0,0 +1,18 @@
+package com.moass.ws.config;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.support.DelegatingIntroductionInterceptor;
+import org.springframework.web.socket.WebSocketSession;
+
+public class SubProtocolWebSocketHandlerInterceptor extends DelegatingIntroductionInterceptor {
+    @Override
+    protected Object doProceed(MethodInvocation mi) throws Throwable {
+        if(mi.getMethod().getName().equals("afterConnectionEstablished")) {
+            WebSocketSession session = (WebSocketSession) mi.getArguments()[0];
+//            session.setTextMessageSizeLimit(50*1024*1024);
+            session.setTextMessageSizeLimit(Integer.MAX_VALUE);
+        }
+
+        return super.doProceed(mi);
+    }
+}
--- Backend/moass-ws-server/src/main/java/com/moass/ws/config/WebSocketConfig.java
+++ Backend/moass-ws-server/src/main/java/com/moass/ws/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,12 +14,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setDecoratorFactories(new AgentWebSocketHandlerDecoratorFactory());
+//        registration.setMessageSizeLimit(50 * 1024 * 1024);
     }
 }
