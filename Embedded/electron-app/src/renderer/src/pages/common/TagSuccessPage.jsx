b7be0f81b350c8316d39de077ecc2981a879257e
서지수 <diduedidue@naver.com>
2024-05-13 05:34:15 +0900

    Style: 로그인 스타일 수정

--- Embedded/electron-app/src/renderer/src/pages/common/TagSuccessPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/common/TagSuccessPage.jsx
@@ -25,9 +25,9 @@ export default function TagSuccess() {
 
   return (
     <div className="pulse-container">
-      <div className="pulse" style={{ animationDelay: '-3s' }}></div>
+      <div className="pulse" style={{ animationDelay: '-6s' }}></div>
+      <div className="pulse" style={{ animationDelay: '-4s' }}></div>
       <div className="pulse" style={{ animationDelay: '-2s' }}></div>
-      <div className="pulse" style={{ animationDelay: '-1s' }}></div>
       <div className="pulse" style={{ animationDelay: '0s' }}></div>
       <div className="flex flex-row justify-center items-center text-8xl w-full h-full z-50">
         {user ? <p>반갑습니다, {user.userName}님!</p> : <p>사용자 정보를 불러오는 중...</p>}
