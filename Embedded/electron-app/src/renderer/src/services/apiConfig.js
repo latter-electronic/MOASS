37df8cca6da023abf6238e7ea0492a6799e10600
RosaDamascena <hsj990604@gmail.com>
2024-05-13 11:00:03 +0900

    Fix: 모찌 점심 메뉴 수정
    api 코드 누락

--- Embedded/electron-app/src/renderer/src/services/apiConfig.js
+++ Embedded/electron-app/src/renderer/src/services/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const MOASS_API_URL = "https://k10e203.p.ssafy.io"
+const MOZZY_LUNCH_URL = "https://ssafyfood-www-jong.koyeb.app"
 
 export function moassApiAxios() {
     return axios.create({
@@ -11,3 +12,9 @@ export function moassApiAxios() {
         },
     });
 }
+
+export function mozzyLunchApiAxios() {
+    return axios.create({
+      baseURL: MOZZY_LUNCH_URL,
+    })
+  }
