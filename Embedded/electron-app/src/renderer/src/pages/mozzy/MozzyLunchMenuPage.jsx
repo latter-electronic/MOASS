829ddf825872e4432c0ab6e222428df512f05d70
RosaDamascena <hsj990604@gmail.com>
2024-05-17 00:16:11 +0900

    Chore: 점심 메뉴 사진 크기 조정

--- Embedded/electron-app/src/renderer/src/pages/mozzy/MozzyLunchMenuPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/mozzy/MozzyLunchMenuPage.jsx
@@ -10,7 +10,7 @@ function LunchMenu() {
     return (
         <div className="flex flex-col items-center justify-center p-1">
             <h1 className="text-2xl font-bold mb-6">오늘의 점심 메뉴</h1>
-            <div className="flex overflow-x-auto bg-white rounded-lg shadow" style={{ maxHeight: '450px', maxWidth: '650px' }}>
+            <div className="flex overflow-x-auto bg-white rounded-lg shadow" style={{ maxHeight: '400px', maxWidth: '650px' }}>
                 <div className="flex flex-row">
                     {lunchMenus.map((menu, index) => (
                         <div key={index} className="flex flex-col justify-between min-w-[650px] p-4">
@@ -19,7 +19,12 @@ function LunchMenu() {
                                 <h2 className="text-xl font-semibold">{menu.fields[1].value}</h2>
                             </div>
                             <div className="flex justify-center items-center">
-                                <img src={menu.image_url} alt={menu.fields[0].value} className="my-6 object-cover w-80"/>
+                                <img 
+                                    src={menu.image_url} 
+                                    alt={menu.fields[0].value} 
+                                    className="my-6 object-cover w-80"
+                                    style={{ height: '250px' }}
+                                />
                             </div>
                         </div>
                     ))}
