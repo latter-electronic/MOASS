8c2a56a30ca430ead4fd67dc72e99434c084e788
www-jong <weon1009@gmail.com>
2024-05-17 15:11:56 +0900

    Add: 호출시 앱에도 알람 추가

--- Backend/moass-api-server/src/main/java/com/moass/api/domain/notification/dto/NotificationSendDto.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/domain/notification/dto/NotificationSendDto.java
@@ -1,10 +1,12 @@
 package com.moass.api.domain.notification.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class NotificationSendDto extends NotificationBase{
 
     private String notificationId;
@@ -16,4 +18,9 @@ public class NotificationSendDto extends NotificationBase{
     public NotificationSendDto(MmParseDto mmParseDto){
         super(mmParseDto.getTitle(),mmParseDto.getSource(), mmParseDto.getBody(),mmParseDto.getSender(),mmParseDto.getIcon());
     }
+
+    public NotificationSendDto(String title, String source, String body, String sender, String icon){
+        super(title, source, body, sender, icon);
+    }
+
 }
