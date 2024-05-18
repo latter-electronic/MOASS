0022d8e4acdb78e7edb634cbc2b9c4390fe39b3b
RosaDamascena <hsj990604@gmail.com>
2024-05-19 01:57:48 +0900

    Add: 오래 앉아 있음 알림 수정
    멘트 수정
    타이머 생성

--- Embedded/electron-app/src/renderer/src/pages/mozzy/MozzyLongSitPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/mozzy/MozzyLongSitPage.jsx
@@ -7,12 +7,29 @@ function LongSit({ isOpen, onClose }) {
         onClose()        // 부모 컴포넌트의 onClose 콜백 실행
     }
 
+    useEffect(() => {
+        let timer;
+        if (isOpen) {
+            // 모달이 열렸을 때 6초 후에 닫히도록 타이머 설정
+            timer = setTimeout(() => {
+                onClose(); // 부모 컴포넌트의 onClose 콜백 실행
+            }, 6000);
+        }
+        
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [isOpen, onClose]);
+
     return (
         <MozzyModal isOpen={isOpen} onRequestClose={handleModalClose}>
             <div className="flex flex-col items-center justify-center text-black p-8">
-                < h2 className="text-2xl font-bold mb-4">
-                    오래 앉아 있었으니 스트레칭을 해주세요!
-                </h2>
+                <p className="text-2xl font-bold mb-4">
+                    알고 계셨나요?
+                </p>
+                <p className="text-2xl font-bold mb-4">
+                    1시간에 한 번씩 스트레칭을 해주는게 좋답니다!
+                </p>
             </div>
         </MozzyModal>
     )
