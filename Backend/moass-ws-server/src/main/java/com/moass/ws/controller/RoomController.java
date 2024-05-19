cd688e3015a51277298879a5aa86ee01cfce020a
이동호 <ghehd1125@gmail.com>
2024-05-19 17:56:53 +0900

    Update Backend/moass-ws-server/src/main/java/com/moass/ws/controller/RoomController.java

--- Backend/moass-ws-server/src/main/java/com/moass/ws/controller/RoomController.java
+++ Backend/moass-ws-server/src/main/java/com/moass/ws/controller/RoomController.java
@@ -44,7 +44,7 @@ public class RoomController {
         Board board = boardService.createBoard(new Board(room));
         room.setId(board.getBoardId());
         roomService.save(room);
-        return ResponseEntity.ok("https://k10e203.p.ssafy.io/room" + board.getBoardId() + "?" + userId);
+        return ResponseEntity.ok("https://k10e203.p.ssafy.io/room/" + board.getBoardId() + "?" + userId);
     }
 
     @PostMapping("/{boardId}/save-drawing")
