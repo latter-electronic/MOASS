c65e68df5c5fbb8c6849077a9768cebcb33a7ecd
Dongho Lee <ghehd1125@gmail.com>
2024-05-20 02:13:38 +0900

    Fix: API 경로 수정

--- Backend/moass-ws-server/src/main/java/com/moass/ws/controller/RoomController.java
+++ Backend/moass-ws-server/src/main/java/com/moass/ws/controller/RoomController.java
@@ -26,7 +26,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/v1/board")
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class RoomController {
 
