744478ae2416c329a8568641af77f2820a73cc92
서지수 <diduedidue@naver.com>
2024-05-20 05:37:43 +0900

    Fix: 알림 자잘한 버그 수정

--- Embedded/electron-app/src/renderer/src/pages/noti/NotiPage.jsx
+++ Embedded/electron-app/src/renderer/src/pages/noti/NotiPage.jsx
@@ -78,7 +78,12 @@ export default function NotiPage() {
                 );
               }
               return (
-                <></>
+                <SwipeableListItem
+                  key={key}
+                  trailingActions={trailingActions(notice.notificationId)}
+                >
+                  <NotiSsafy notice={notice} />
+                </SwipeableListItem>
               );
             })}
           </SwipeableList>
--- Embedded/electron-app/src/renderer/src/stores/notiStore.js
+++ Embedded/electron-app/src/renderer/src/stores/notiStore.js
@@ -1,13 +1,21 @@
 import { create } from 'zustand';
 
-const useNotiStore = create((set) => ({
+const useNotiStore = create((set, get) => ({
     notifications: [],
-    addNotification: (notification) => set((state) => ({
-        notifications: [notification, ...state.notifications]  // 알림을 배열의 맨 앞에 추가
-    })),
-    addNotifications: (notifications) => set((state) => ({
-        notifications: [...notifications, ...state.notifications].sort((a, b) => new Date(b.date) - new Date(a.date))  // 다중 알림을 추가하고 정렬
-    })),
+    addNotification: (notification) => set((state) => {
+        const isDuplicate = state.notifications.some(noti => noti.body === notification.body);
+        if (isDuplicate) {
+            return { notifications: state.notifications };
+        }
+        return { notifications: [notification, ...state.notifications] };
+    }),
+    addNotifications: (notifications) => set((state) => {
+        const existingNotificationBodies = state.notifications.map(noti => noti.body);
+        const uniqueNotifications = notifications.filter(notification => {
+            return !existingNotificationBodies.includes(notification.body);
+        });
+        return { notifications: [...uniqueNotifications, ...state.notifications] };
+    }),
     clearNotifications: () => set({ notifications: [] })
 }));
 
