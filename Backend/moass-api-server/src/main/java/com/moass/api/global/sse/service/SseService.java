a6b42d1396ef292fc51e077e6865463f0b8456d7
www-jong <weon1009@gmail.com>
2024-05-24 03:48:02 +0900

    Refactor: 서버상태로그 1분으로 간격 늘림

--- Backend/moass-api-server/src/main/java/com/moass/api/global/sse/service/SseService.java
+++ Backend/moass-api-server/src/main/java/com/moass/api/global/sse/service/SseService.java
@@ -161,7 +161,7 @@ public class SseService {
         }
     }
 
-    @Scheduled(fixedRate = 30000)  // 10 seconds
+    @Scheduled(fixedRate = 60000)  // 10 seconds
     public void sendTestMessages() {
         log.info("userSinks size: {}", userSinks.size());
         log.info("teamSinks size: {}", teamSinks.size());
