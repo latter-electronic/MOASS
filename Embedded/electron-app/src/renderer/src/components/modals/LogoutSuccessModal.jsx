59d443be1242d17c967372ad453d22e31b408c83
서지수 <diduedidue@naver.com>
2024-06-02 16:51:06 +0900

    Feat: n초후 자동으로 닫힘 기능 추가

--- Embedded/electron-app/src/renderer/src/components/modals/LogoutSuccessModal.jsx
+++ Embedded/electron-app/src/renderer/src/components/modals/LogoutSuccessModal.jsx
@@ -1,18 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
 export default function LogoutSuccessModal({ onClose }) {
-    return (
-      <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div className="bg-white p-7 rounded-lg shadow-lg text-zinc-900">
-          <h2 className="text-xl font-bold mb-4">로그아웃 성공</h2>
-          <p className="text-lg mb-4">로그아웃이 성공적으로 완료되었습니다.</p>
-          <div className="flex justify-center mt-6">
-            <button
-              className="bg-green-100 text-lg hover:bg-green-700 text-green-600 font-semibold py-2 px-4 rounded-2xl w-36"
-              onClick={onClose}
-            >
-              닫기
-            </button>
-          </div>
+  const [secondsLeft, setSecondsLeft] = useState(3);
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      onClose();
+      return;
+    }
+    const timer = setInterval(() => {
+      setSecondsLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [secondsLeft, onClose]);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-white p-7 rounded-lg shadow-lg text-zinc-900">
+        <h2 className="text-xl font-bold mb-4">로그아웃 성공!</h2>
+        <p className="text-lg mb-4">다음에도 모아쓰와 함께해주세요🖐🏻</p>
+        <div className="flex justify-center mt-6">
+          <button
+            className="bg-green-100 text-lg hover:bg-green-700 text-green-600 font-semibold py-2 px-4 rounded-2xl flex-grow"
+            onClick={onClose}
+          >
+            {secondsLeft}초 후 자동으로 닫힘
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
